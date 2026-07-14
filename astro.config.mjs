e75e4e8792f736c5f86b351d68c484340441db3d
Arushi Jain <ars.jain99@gmail.com>
2026-07-13 17:30:45 -0700

    revert: undo github deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-    site: 'https://ars1999.github.io',
-    base: '/personal-website',
-});
+export default defineConfig({});
