7ab57c9b4d14c187c08ce8df64cd4cf62e00f771
Arushi Jain <ars.jain99@gmail.com>
2026-07-13 17:25:13 -0700

    chore: add deployment data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ars1999.github.io',
+    base: '/personal-website',
+});
